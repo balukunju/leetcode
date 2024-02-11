b749786ddd01b90b18b6b692f30c59ddb418544e
balukunju <balukunju@gmail.com>
2024-02-11 13:27:15 -0800

    Merge Sort

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -13,3 +13,10 @@ class Solution:
         return b
 # @lc code=end
 
+        for i in range(1, len(nums)):
+            j = i - 1
+            while (j >= 0 and nums[j+1] < nums[j]):
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+                j -= 1
+        return nums
+        
---
+++ 912.sort-an-array.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=912 lang=python3
+#
+# [912] Sort an Array
+#
+
+# @lc code=start
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def mergeSort(A):
+            LA = len(A)
+            if (LA == 1):
+                return A
+            LHA, RHA = mergeSort(A[:LA//2]), mergeSort(A[LA//2:])
+            return merge(LHA, RHA)
+        
+        def merge(LH, RH):
+            LLH, LRH = len(LH), len(RH)
+            S, i, j = [], 0, 0
+            while (i < LLH and j < LRH):
+                if LH[i] <= RH[j]:
+                    i, _ = i+1, S.append(LH[i])
+                else:
+                    j, _ = j+1, S.append(RH[j])
+            return S + (RH[j:] if i == LLH else LH[i:])
+        
+        return mergeSort(nums)        
+        
+# @lc code=end
+
