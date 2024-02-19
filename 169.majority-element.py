1ac2779c48842c0c98b01a53d691e3d7543ad85c
balukunju <balukunju@gmail.com>
2024-02-18 17:13:17 -0800

    Mjaroty Element

---
+++ 169.majority-element.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=169 lang=python3
+#
+# [169] Majority Element
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if candidate == num else -1
+        return candidate
+# @lc code=end
+
